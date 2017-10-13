577280c8ae6eb7f05b61b9b833db43c0d1e5a7ff
Nguyen Nhu Hieu <hieumoscow89@yahoo.com>
2017-10-13 15:05:43 +0800

    add XamlReader

--- Xexpo/Xexpo/MainPage.xaml.cs
+++ Xexpo/Xexpo/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xexpo.Utils;
 
 namespace Xexpo
 {
@@ -12,6 +13,14 @@ namespace Xexpo
         public MainPage()
         {
             InitializeComponent();
+            XAMLBox.Text = "<?xml version=\"1.0\" encoding=\"UTF - 8\"?>\r\n<ContentView xmlns = \"http://xamarin.com/schemas/2014/forms\" xmlns:x = \"http://schemas.microsoft.com/winfx/2009/xaml\" >\r\n\t<ContentView.Content>\r\n\t\t<StackLayout VerticalOptions=\"Center\" HorizontalOptions=\"Center\" BackgroundColor=\"LightBlue\">\r\n\t\t\t<Label Text=\"Hello Xamarin.Forms!\" />\r\n\t\t</StackLayout>\r\n\t</ContentView.Content>\r\n</ContentView>";
+            RenderButton.Clicked += RenderButton_Clicked;
+        }
+
+
+        private void RenderButton_Clicked(object sender, EventArgs e)
+        {
+            ContentBox.Content = XamlReader.Load<ContentView>(XAMLBox.Text);
         }
     }
 }
---
+++ Xexpo/Xexpo/Utils/XamlReader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using xaml = Xamarin.Forms.Xaml;
+
+namespace Xexpo.Utils
+{
+    /// <summary>
+    /// Dynamically converts XAML in to UI elements
+    /// </summary>
+    public static class XamlReader
+    {
+        #region Fields
+        private static MethodInfo _FirstExtensionsMethods;
+        private const string ClrNamespaceParamName = "clrNamespace";
+        #endregion
+
+        #region Static Constructor
+        private static void Initialise()
+        {
+            // This is the current situation, where the LoadFromXaml is the only non-public static method.
+            _FirstExtensionsMethods = typeof(xaml.Extensions).GetTypeInfo().DeclaredMethods.First(x => x.IsPublic == false && x.IsStatic);
+
+            _FirstExtensionsMethods = _FirstExtensionsMethods?.MakeGenericMethod(typeof(BindableObject)) ?? throw new NotSupportedException("Xamarin.Forms implementation of XAML loading not found. Please update the Dynamic nuget package.");
+        }
+        #endregion
+
+        #region Private Static Methods
+        private static TBindableObject Load<TBindableObject>(this TBindableObject view, string xaml) where TBindableObject : BindableObject
+        {
+            try
+            {
+                if (_FirstExtensionsMethods == null)
+                {
+                    Initialise();
+                }
+
+                return (TBindableObject)_FirstExtensionsMethods.Invoke(null, new object[] { view, xaml });
+            }
+            catch (Exception ex)
+            {
+                var anex = ex.InnerException as ArgumentNullException;
+                if (anex == null)
+                {
+                    throw;
+                }
+
+                if (anex.ParamName == ClrNamespaceParamName)
+                {
+                    throw new Exception($"The XAML parser did not receive the argument {ClrNamespaceParamName}. This probably indicates that a default namespace was not specified in the XAML.\r\n\r\nXaml:\r\n{xaml}");
+                }
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Public Static Methods
+        /// <summary>
+        /// Dynamically loads Xamarin Forms xaml in to a BindableObject of the type specified
+        /// </summary>
+        /// <typeparam name="TBindableObject">This must be a type that inherits from BindableObject</typeparam>
+        /// <param name="xaml">Xamarin Forms XAML. Note: the root element must be of the same type specified in the <typeparamref name="TBindableObject"/> generic argument.</param>
+        /// <returns>The BindableObject visual tree</returns>
+        public static TBindableObject Load<TBindableObject>(string xaml) where TBindableObject : BindableObject
+        {
+            if (string.IsNullOrEmpty(xaml))
+            {
+                throw new ArgumentNullException(nameof(xaml));
+            }
+
+            var bindableObject = (TBindableObject)Activator.CreateInstance(typeof(TBindableObject));
+
+            return Load(bindableObject, xaml);
+        }
+        #endregion
+    }
+}
